f12bf97517af0ccde83380beac94895b2b7c69d9
Miki Namsrai <miki.namsrai@gmail.com>
2023-03-06 18:49:17 +0100

    Add Quicksand font

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import reactLogo from './assets/react.svg'
 function App() {
 	const [count, setCount] = useState(0)
 
-	return <div className="App bg-slate-600">Yo</div>
+	return (
+		<div className="App text-center text-base font-quicksand">
+			<h1 className="text-xl px-5 border-b-2 border-b-slate-100 text-gray-500">
+				Motdle
+			</h1>
+		</div>
+	)
 }
 
 export default App
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'quicksand': 'Quicksand, Arial, sans-serif',
+      }
+    },
   },
   plugins: [],
 }

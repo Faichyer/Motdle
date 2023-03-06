973c7cc782dc19cef594580727f474d8dd50c892
Miki Namsrai <miki.namsrai@gmail.com>
2023-03-06 22:03:01 +0100

    add Motdle hook

---
+++ src/hooks/useMotdle.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+type MotdleProps = {}
+
+function useMotdle({}: MotdleProps) {
+	const [turn, setTurn] = useState(0)
+	const [currentGuess, setCurrentGuess] = useState('')
+	const [guesses, setGuesses] = useState([])
+	const [history, setHistory] = useState([])
+	const [isCorrect, setIsCorrect] = useState(false)
+
+	const formatGuess = () => {}
+
+	const addNewGuess = () => {}
+
+	const handleKeyup = () => {}
+
+	return { turn, currentGuess, guesses, isCorrect, handleKeyup }
+}
+
+export default useMotdle

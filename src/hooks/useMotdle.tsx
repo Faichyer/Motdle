3df05bf80afd066c16fee2d2974f533ef4d72efa
Miki Namsrai <miki.namsrai@gmail.com>
2023-04-01 23:16:42 +0200

    Submitting text

--- src/hooks/useMotdle.tsx
+++ src/hooks/useMotdle.tsx
@@ -9,22 +9,43 @@ function useMotdle({ solution }: MotdleProps) {
 	const [turn, setTurn] = useState(0)
 	const [currentGuess, setCurrentGuess] = useState('')
 	const [guesses, setGuesses] = useState([])
-	const [history, setHistory] = useState([])
+	const [history, setHistory] = useState([] as String[])
 	const [isCorrect, setIsCorrect] = useState(false)
 
-	const formatGuess = () => {}
+	const formatGuess = () => {
+		console.log('formatting the current guess - ' + currentGuess)
+	}
 
 	const addNewGuess = () => {}
 
 	const handleKeyup = ({ key }: KeyboardEvent) => {
+		// Submitting
 		if (key === 'Enter') {
+			if (turn > 5) {
+				console.log('Tu as utilisé tous tes tours')
+				return
+			}
+
+			if (history.includes(currentGuess)) {
+				console.log('Tu as déjà essayé ce mot')
+				return
+			}
+
+			if (currentGuess.length !== 5) {
+				console.log('Le mot doit avoir 5 caractères')
+				return
+			}
+
+			formatGuess()
 		}
 
+		// Backspace to erase
 		if (key === 'Backspace') {
 			setCurrentGuess((prev) => prev.slice(0, -1))
 			return
 		}
 
+		// Guess submit
 		if (/^[A-Za-z]$/.test(key)) {
 			if (currentGuess.length < 5) {
 				setCurrentGuess((prev) => prev + key)
